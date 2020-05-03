9acb4d8275b48b7ed067555eb67da89a999949cc
Kacper Dabrowski <kacperdabrowski@MacBook-Pro-Kacper.local>
2020-05-03 11:59:45 +0200

    Questions mechanism implemented

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,35 @@
 const name = prompt('Give me your name, so I could sign you up for the quiz');
-if (!name) location.reload();
 const socket = io();
+let answer = null;
 
-socket.emit('introduce', name);
+const answerInput = document.querySelector('.answers');
+answerInput.addEventListener('change', (e) => {
+    answer = e.target.value;
+    console.log(answer);
+})
+socket.on('welcome', (message) => {
+    console.log(message)
+})
+socket.emit('introduce', {name});
+socket.on('question', (data) => {
+    console.log(data)
+    answer = null;
+    answerInput.value = null;
+    const questionContent = document.querySelector('.question-content');
+    const answers = document.querySelector('.answers');
+    const answersOptions = document.querySelectorAll('.answers option')
+    const answerListItems = document.querySelectorAll('ul li');
+    questionContent.textContent = data.question;
+    for (let i = 0; i < data.answers.length; i++) {
+        answerListItems[i].textContent = data.answers[i];
+        answersOptions[i].value = data.answers[i];
+        answersOptions[i].textContent = data.answers[i];
+
+    }
+
+
+})
+document.querySelector('button').addEventListener('click', () => {
+    console.log(answer, ' sent')
+    socket.emit('answer', {username: name, answer: answer});
+})
--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const server = http.Server(app);
 const io = require('socket.io')(server);
 const questionRouter = require('./routes/questions');
 const Question = require('./models/Question');
+const Player = require('./models/Player')
 
 const players = [];
 const questionQueue = Question.getAllQuestions();
@@ -15,16 +16,44 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use('/api', questionRouter);
 io.on('connection', (socket) => {
-  playersCount += 1;
-  io.emit('welcome', 'Welcome to the server!');
-  socket.on('introduce', (data) => {});
-  socket.on('disconnect', () => {
-    playersCount -= 1;
-    console.log('User left the game!');
-    console.log(playersCount);
-  });
+    playersCount += 1;
+    io.emit('welcome', 'Welcome to the server!');
+    socket.on('introduce', (data) => {
+        const player = new Player(data.name);
+        players.push(player);
+        console.log(players);
+        socket.name = data.name;
+        if (players.length > 1) {
+            console.log('Game starts')
+            emitQuestions(io, socket, questionQueue).then(data => console.log('data'))
+        }
+    });
+
+
+    socket.on('disconnect', () => {
+        playersCount -= 1;
+        console.log('User left the game!');
+        console.log(playersCount);
+
+    });
 });
-io.on('', () => {});
+
 server.listen(8080, () => {
-  console.log('App is running on 8080');
+    console.log('App is running on 8080');
 });
+const sleep = (ms) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+const emitQuestions = async (io, socket, questions) => {
+    for (let i = 0; i < questions.length; i++) {
+        io.emit('question', {question: questions[i].question, answers: questions[i].answers});
+        socket.on('answer', (data) => {
+            console.log(data.username, ' answered ', data.answer)
+        })
+        await sleep(10000);
+
+
+    }
+}
